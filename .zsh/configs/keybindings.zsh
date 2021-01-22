ef24e1248efa95b3663c1bdbc4b2e0a379894aac
b08x <rwpannick@gmail.com>
2021-01-21 19:52:17 -0500

    several things

--- .zsh/configs/keybindings.zsh
+++ .zsh/configs/keybindings.zsh
@@ -7,7 +7,7 @@ bindkey "^F" vi-cmd-mode
 
 # handy keybindings
 bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
+bindkey '^[[3~' end-of-line
 bindkey "^K" kill-line
 bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
