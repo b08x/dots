d4f39ab1f1d17e13d40ad9db61a8cd8ef8d3027a
Robert Pannick <rwpannick@gmail.com>
2025-05-29 09:03:22 -0400

    feat: Added bootstrap script for initial setup
    This commit introduces a new bootstrap script (02-setup.sh) to automate the initial setup process. The script performs the following actions:
    
    - Sets up environment variables.
    - Installs prerequisite packages using pacman.
    - Installs a list of ruby gems.
    - Clones or updates the syncopated ansible configuration.
    - Creates an ansible inventory file.
    - Runs the ansible playbook to configure the system.
    
    Signed-off-by: Robert Pannick <rwpannick@gmail.com>

---
+++ .config/yadm/bootstrap.d/02-setup.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+source $HOME/.config/yadm/bootstrap.d/gum_wrapper.sh
+
+DISTRO=$( hostnamectl| awk '{ print $1,$2,$3 }'|grep "Operating System"|awk '{print $3}' )
+
+export PATH+=":$HOME/.local/share/gem/ruby/3.4.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+export ANSIBLE_HOME="$HOME/.config/syncopated"
+export ANSIBLE_PLUGINS="$ANSIBLE_HOME/plugins/modules"
+export ANSIBLE_CONFIG="$ANSIBLE_HOME/ansible.cfg"
+export ANSIBLE_INVENTORY="$ANSIBLE_HOME/inventory.ini"
+
+BOOTSTRAP_PKGS=(
+  'ansible'
+  'base-devel'
+  'ccache'
+  'cmake'
+  'git'
+  'htop'
+  'neovim'
+  'net-tools'
+  'python-pip'
+  'reflector'
+  'ruby-bundler'
+  'rubygems'
+  'rust'
+  'wget'
+  'zsh'
+)
+
+# install pre-requisite packages
+sudo pacman -S --noconfirm --needed "${BOOTSTRAP_PKGS[@]}"
+
+# install ruby gems
+echo "gem: --user-install --no-document" | sudo tee /root/.gemrc
+
+INSTALLED_GEMS=$(gem list | awk '{ print $1 }')
+
+GEMS=(
+  'activesupport'
+  'awesome_print'
+  'bcrypt_pbkdf'
+  'childprocess'
+  'ed25519'
+  'eventmachine'
+  'ffi'
+  'fractional'
+  'geo_coord'
+  'highline'
+  'i3ipc'
+  'i18n'
+  'kramdown'
+  'logging'
+  'minitest'
+  'mocha'
+  'multi_json'
+  'net-ssh'
+  'parallel'
+  'pastel'
+  'pry'
+  'pry-doc'
+  'pycall'
+  'rake'
+  'rdoc'
+  'rexml'
+  'rouge'
+  'sync'
+  'sys-proctable'
+  'tty-box'
+  'tty-command'
+  'tty-cursor'
+  'tty-prompt'
+  'tty-screen'
+  'tty-tree'
+)
+
+# https://stackoverflow.com/a/42399479
+mapfile -t DIFF < \
+    <(comm -23 \
+        <(IFS=$'\n'; echo "${GEMS[*]}" | sort) \
+        <(IFS=$'\n'; echo "${INSTALLED_GEMS[*]}" | sort) \
+    )
+
+export GEM_HOME="${HOME}/.gem"
+
+for gem in "${DIFF[@]}"; do
+  gem install "$gem" || continue
+done
+
+if [ ! -d $ANSIBLE_HOME ]; then
+  git clone --recursive git@github.com:syncopatedX/ansible.git $ANSIBLE_HOME
+  cd $ANSIBLE_HOME && git checkout development
+else
+  echo "syncopated config already exists."
+  cd $ANSIBLE_HOME && git checkout development && git fetch && git pull
+fi
+
+echo "$(uname -n) ansible_user=$USER ansible_connection=local" > $ANSIBLE_INVENTORY
+
+ansible-playbook -K -c local -i "${ANSIBLE_INVENTORY}", "${ANSIBLE_HOME}/playbooks/full.yml" \
+                -e "newInstall=true" \
+                -e "cleanup=true"
+
+
